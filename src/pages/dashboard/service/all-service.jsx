d04baf9d8e31b716efbf1941db350041156a366a
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-13 15:01:28 +0600

    feat: service update page done

--- src/pages/dashboard/service/all-service.jsx
+++ src/pages/dashboard/service/all-service.jsx
@@ -1,10 +1,9 @@
-import Loader from "@/components/UI/Loader";
 import {
   useDeleteServiceMutation,
   useGetAllServiceQuery,
 } from "@/redux/service/serviceApi";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
 import useProtectedRoute from "../../../hooks/useProtectedRoute";
@@ -13,8 +12,7 @@ import DashboardLayout from "../../../layouts/DashboardLayout";
 const jwt = require("jsonwebtoken");
 
 const AllServices = () => {
-  const [testCategory, setTestCategory] = useState("English");
-  const { data: allTest } = useGetAllServiceQuery();
+  const { data: allService } = useGetAllServiceQuery();
 
   const accessToken =
     typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
@@ -27,84 +25,48 @@ const AllServices = () => {
     authorization: accessToken,
   };
 
-  const testUniqueSubjects = [];
-  allTest?.data?.map((test) => {
-    if (testUniqueSubjects?.length > 0) {
-      const isSubjectExist = testUniqueSubjects?.find(
-        (item) => item?.subject === test?.subject
-      );
-
-      if (!isSubjectExist) {
-        testUniqueSubjects.push(test);
-      }
-    } else {
-      testUniqueSubjects.push(test);
-    }
-  });
-
-  const filterByTestSubject = allTest?.data?.filter((exam) => {
-    return exam?.subject === testCategory;
-  });
-
-  const [deleteTest, { isError, isLoading, isSuccess, error }] =
+  const [deleteService, { isError, isLoading, isSuccess, error }] =
     useDeleteServiceMutation();
 
-  const handleDeleteTest = (test) => {
+  const handleDeleteService = (service) => {
     const isConfirm = window.confirm(
-      `Do you want to delete: ${test?.subject} ${test?.serial}`
+      `Do you want to delete: ${service?.title}`
     );
     if (isConfirm) {
-      deleteTest({ id: test?.id, headers });
+      deleteService({ id: service?.id, headers });
     }
   };
 
   return (
     <div>
       <div className="my-20 w-11/12 md:w-10/12 mx-auto">
-        <h1 className="text-3xl font-semibold text-center my-8">Tests</h1>
-        {testUniqueSubjects && testUniqueSubjects.length > 0 ? (
-          <div className="w-10/12 md:w-8/12 mx-auto flex flex-wrap justify-around gap-3">
-            {testUniqueSubjects?.map((test, index) => (
-              <div
-                key={index}
-                onClick={() => setTestCategory(test?.subject)}
-                className={`relative mb-2 min-w-[96px] text-center hover:bg-blue-500 px-2 py-1 rounded-md cursor-pointer hover:text-white font-semibold ${
-                  testCategory === test?.subject
-                    ? "bg-blue-500"
-                    : "bg-[#1d1836]"
-                }`}
-              >
-                {testCategory === test?.subject && (
-                  <div className="absolute top-full left-[45%] triangle_down border-l-[7px] border-r-[7px] border-t-[10px] border-t-blue-500 border-transparent"></div>
-                )}
-                {test?.subject}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <Loader />
-        )}
+        <h1 className="text-3xl font-semibold text-center my-8">Services</h1>
         <div className="mt-10 flex flex-col gap-5">
-          {filterByTestSubject?.map((test, index) => (
+          {allService?.data?.map((service, index) => (
             <div
               key={index}
               className="flex justify-between items-center  bg-[#1d1836] p-2 rounded-md"
             >
               <div>
                 <h4 className="text-md font-semibold">
-                  {test?.subject} {test?.serial}
+                  {service?.subject} {service?.serial}
                 </h4>
-                <p>Question: {test?.questions?.length}</p>
-                <p>Time: {test?.timeLimit} min</p>
+                <p>Title: {service?.title}</p>
+                <p>
+                  Description:{" "}
+                  {service?.description.length > 20
+                    ? `${service?.description?.slice(0, 20)}...`
+                    : service?.description}
+                </p>
               </div>
               <div className="flex flex-col items-center justify-between gap-4">
-                <Link href={`/dashboard/test/update/${test?.id}`}>
+                <Link href={`/dashboard/service/update/${service?.id}`}>
                   <button className="text-lg border-none text-primary hover:text-blue-600">
                     <FaRegEdit />
                   </button>
                 </Link>
                 <button
-                  onClick={() => handleDeleteTest(test)}
+                  onClick={() => handleDeleteService(service)}
                   className="text-2xl border-none  text-red-500 hover:text-red-600"
                 >
                   <MdDeleteOutline />
---
+++ src/pages/dashboard/service/update/[serviceId].jsx
@@ -0,0 +1,121 @@
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import {
+  useGetSingleServiceQuery,
+  useUpdateServiceMutation,
+} from "../../../../redux/service/serviceApi";
+import DashboardLayout from "../../../../layouts/DashboardLayout";
+import useProtectedRoute from "../../../../hooks/useProtectedRoute";
+import { useRouter } from "next/router";
+
+const jwt = require("jsonwebtoken");
+
+const UpdateService = () => {
+  const { query } = useRouter();
+  const id = query.serviceId;
+
+  const accessToken =
+    typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
+  const decodedToken = jwt.decode(accessToken);
+
+  const headers = {
+    authorization: accessToken,
+  };
+
+  const { data: getSingleService } = useGetSingleServiceQuery(id);
+  console.log(getSingleService?.data);
+
+  const [
+    updateService,
+    { data, isError, isLoading, isSuccess, error, status },
+  ] = useUpdateServiceMutation();
+
+  // Protect Route
+  useProtectedRoute(decodedToken?.role || "guest");
+
+  const handleUpdateService = (e) => {
+    e.preventDefault();
+    const data = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      location: e.target.location.value,
+      price: parseInt(e.target.price.value),
+      image: e.target.image.value,
+    };
+
+    updateService({ id, data, headers });
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(`${error?.data?.message}` || "Service Creation Failed!");
+    }
+
+    if (isSuccess) {
+      toast.success("Service Updated Successfully!");
+    }
+  }, [isLoading, isSuccess, isError, error]);
+
+  return (
+    <div className="my-5">
+      <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto mt-5 border rounded-lg border-blue-500 p-5">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-center mb-5">
+          Update Service
+        </h3>
+        <form
+          onSubmit={(e) => handleUpdateService(e)}
+          className="grid grid-cols-1 justify-between gap-6 mt-4"
+        >
+          <input
+            type="text"
+            name="image"
+            placeholder="Image URL"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            defaultValue={getSingleService?.data?.image}
+            required
+          />
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            defaultValue={getSingleService?.data?.title}
+            required
+          />
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            defaultValue={getSingleService?.data?.price}
+            required
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            defaultValue={getSingleService?.data?.location}
+            required
+          />
+          <textarea
+            className="textarea first-letter:input input-bordered input-primary input-sm w-full h-[150px] bg-[#1d1836]"
+            name="description"
+            placeholder="Description"
+            defaultValue={getSingleService?.data?.description}
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-sm btn-primary">
+            Update Service
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateService;
+
+UpdateService.getLayout = function getLayout(page) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
