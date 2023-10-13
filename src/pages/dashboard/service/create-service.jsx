83e85018fd7691e464b607f80f1c781ed053b350
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-13 17:13:47 +0600

    update: service create functionality update

--- src/pages/dashboard/service/create-service.jsx
+++ src/pages/dashboard/service/create-service.jsx
@@ -1,6 +1,5 @@
 import { useCreateServiceMutation } from "@/redux/service/serviceApi";
-import { useGetMyProfileQuery } from "@/redux/user/userApi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import DashboardLayout from "../../../layouts/DashboardLayout";
 import useProtectedRoute from "../../../hooks/useProtectedRoute";
@@ -8,6 +7,8 @@ import useProtectedRoute from "../../../hooks/useProtectedRoute";
 const jwt = require("jsonwebtoken");
 
 const CreateService = () => {
+  const [serviceData, setServiceData] = useState({});
+  const [facilities, setFacilities] = useState([]);
   const accessToken =
     typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
   const decodedToken = jwt.decode(accessToken);
@@ -20,27 +21,46 @@ const CreateService = () => {
     createService,
     { data, isError, isLoading, isSuccess, error, status },
   ] = useCreateServiceMutation();
-  const { data: getMyProfile } = useGetMyProfileQuery({ headers });
 
   // Protect Route
   useProtectedRoute(decodedToken?.role || "guest");
 
-  const handleCreateService = (e) => {
+  const handleAddFacilities = (e) => {
+    e.preventDefault();
+    const facility = e.target.facility.value;
+    setFacilities([...facilities, facility]);
+    setServiceData({ ...serviceData, facilities: [...facilities, facility] });
+    e.target.facility.value = "";
+  };
+  console.log(serviceData);
+
+  const handleAddServiceData = (e) => {
     e.preventDefault();
     const data = {
-      title: e.target.title.value,
+      type: e.target.type.value,
       description: e.target.description.value,
       location: e.target.location.value,
       price: parseInt(e.target.price.value),
       image: e.target.image.value,
     };
+    setServiceData({
+      ...serviceData,
+      type: e.target.type.value,
+      description: e.target.description.value,
+      location: e.target.location.value,
+      price: parseInt(e.target.price.value),
+      image: e.target.image.value,
+    });
 
-    createService({ data, headers });
     e.target.image.value = "";
-    e.target.title.value = "";
+    e.target.type.value = "";
     e.target.price.value = 0;
     e.target.location.value = "";
     e.target.description.value = "";
+  };
+
+  const handleCreateService = () => {
+    createService({ data: serviceData, headers });
   };
 
   useEffect(() => {
@@ -57,23 +77,50 @@ const CreateService = () => {
     <div className="my-5">
       <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto mt-5 border rounded-lg border-blue-500 p-5">
         <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-center mb-5">
-          Create A Service
+          Service Data
         </h3>
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2">
+            {serviceData?.type && (
+              <li className="list-disc">
+                <strong className="text-blue-500">Type: </strong>
+                {serviceData?.type}
+              </li>
+            )}
+            {serviceData?.price && (
+              <li className="list-disc">
+                <strong className="text-blue-500">Price: </strong>
+                {serviceData?.price}
+              </li>
+            )}
+            {serviceData?.location && (
+              <li className="list-disc">
+                <strong className="text-blue-500">Location: </strong>
+                {serviceData?.location}
+              </li>
+            )}
+            {serviceData?.image && (
+              <li className="list-disc">
+                <strong className="text-blue-500">Image: </strong>
+                {serviceData?.image}
+              </li>
+            )}
+          </div>
+          {serviceData?.description && (
+            <li className="list-disc">
+              <strong className="text-blue-500">Description: </strong>
+              {serviceData?.description}
+            </li>
+          )}
+        </div>
         <form
-          onSubmit={(e) => handleCreateService(e)}
+          onSubmit={(e) => handleAddServiceData(e)}
           className="grid grid-cols-1 justify-between gap-6 mt-4"
         >
           <input
             type="text"
-            name="image"
-            placeholder="Image URL"
-            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
-            required
-          />
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
+            name="type"
+            placeholder="Type"
             className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
             required
           />
@@ -91,6 +138,13 @@ const CreateService = () => {
             className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
             required
           />
+          <input
+            type="text"
+            name="image"
+            placeholder="Image URL"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            required
+          />
           <textarea
             className="textarea first-letter:input input-bordered input-primary input-sm w-full h-[150px] bg-[#1d1836]"
             name="description"
@@ -98,9 +152,44 @@ const CreateService = () => {
             required
           ></textarea>
           <button type="submit" className="btn btn-sm btn-primary">
-            Create Service
+            Add Service Data
           </button>
         </form>
+      </div>
+      <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto mt-5 border rounded-lg border-blue-500 p-5">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-center mb-5">
+          Add facilities
+        </h3>
+        <div>
+          {facilities?.map((facility, index) => (
+            <li className="list-disc" key={index}>
+              {facility}
+            </li>
+          ))}
+        </div>
+        <form
+          onSubmit={(e) => handleAddFacilities(e)}
+          className="grid grid-cols-1 justify-between gap-6 mt-4"
+        >
+          <input
+            type="text"
+            name="facility"
+            placeholder="Add a facility"
+            className="input input-bordered input-primary input-sm w-full bg-[#1d1836]"
+            required
+          />
+          <button type="submit" className="btn btn-sm btn-primary">
+            Add a Facility
+          </button>
+        </form>
+      </div>
+      <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto mt-5">
+        <button
+          onClick={() => handleCreateService()}
+          className="btn btn-sm btn-primary my-5 w-full"
+        >
+          Create Service
+        </button>
       </div>
     </div>
   );
