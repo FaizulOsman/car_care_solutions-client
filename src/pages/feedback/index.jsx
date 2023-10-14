4dc9358af9f49235843d25224dd36c0f9db11f43
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-15 00:49:34 +0600

    feat: feadback functionality done

--- src/pages/feedback/index.jsx
+++ src/pages/feedback/index.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RootLayout from "../../layouts/RootLayout";
+import { useCreateFeedbackMutation } from "../../redux/feedback/feedbackApi";
+import toast from "react-hot-toast";
+
+const jwt = require("jsonwebtoken");
 
 const FeedbackPage = () => {
+  const accessToken =
+    typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
+
+  const decodedToken = jwt.decode(accessToken);
+
+  const headers = {
+    authorization: accessToken,
+  };
+  const [createFeedback, { isSuccess, isError, error }] =
+    useCreateFeedbackMutation();
+
+  const handleFeedbackSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: decodedToken?.email || e.target.email.value,
+      message: e.target.message.value,
+    };
+    createFeedback({ data, headers });
+    e.target.message.value = "";
+    e.target.email.value = "";
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Feedback successfully sent!");
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message || "Something went wrong!");
+    }
+  }, [isError, error]);
+
   return (
     <div>
       <div class="max-w-xl mx-auto mt-16 flex w-full flex-col border rounded-lg bg-white p-8">
@@ -11,30 +49,35 @@ const FeedbackPage = () => {
         <p class="mb-5 leading-relaxed text-gray-600">
           If you had any issues or you liked our services, please share with us!
         </p>
-        <div class="mb-4">
-          <label for="email" class="text-sm leading-7 text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            class="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="message" class="text-sm leading-7 text-gray-600">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            class="h-32 w-full resize-none rounded border border-gray-300 bg-white py-1 px-3 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-          ></textarea>
-        </div>
-        <button class="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-          Send
-        </button>
+        <form onSubmit={(e) => handleFeedbackSubmit(e)}>
+          <div class="mb-4">
+            <label for="email" class="text-sm leading-7 text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="message" class="text-sm leading-7 text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              class="h-32 w-full resize-none rounded border border-gray-300 bg-white py-1 px-3 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            class="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none"
+          >
+            Send
+          </button>
+        </form>
         <p class="mt-3 text-xs text-gray-500">
           Feel free to connect with us on social media platforms.
         </p>
