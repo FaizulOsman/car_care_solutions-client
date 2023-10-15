d76dd4641ab9185dc191b0f746df4451239f97dd
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-16 00:54:36 +0600

    update: README.md file updated

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -94,7 +94,7 @@ const SignUp = () => {
           </div>
           <div className="mb-2">
             <input
-              type="confirmPassword"
+              type="password"
               name="confirmPassword"
               placeholder="Confirm Password"
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
