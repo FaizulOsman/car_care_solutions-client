788408cc85312890904e28f10e3bf34e48b9c32a
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-30 12:29:00 +0600

    update: service card updated

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -5,6 +5,7 @@ import RootLayout from "../../layouts/RootLayout";
 import toast from "react-hot-toast";
 import { useCreateAddToCartMutation } from "../../redux/addToCart/addToCartApi";
 import Loader from "../../components/UI/Loader";
+import { RxCross2 } from "react-icons/rx";
 
 const jwt = require("jsonwebtoken");
 
@@ -38,6 +39,15 @@ const ServicesPage = () => {
     createAddToCart({ data, headers });
   };
 
+  let facilities = [
+    "Exterior Hand Wash",
+    "Rims + Tire Shine",
+    "Full Vacuum",
+    "Shampoo Seats",
+    "Interior Wipe Down",
+    "Deep Leather Cleaning",
+  ];
+
   let ongoingServices = allService?.data?.filter(
     (data) => data.status === "ongoing"
   );
@@ -67,7 +77,7 @@ const ServicesPage = () => {
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search for services"
-            className="input input-sm sm:input-md input-bordered input-primary w-full max-w-xs"
+            className="input input-sm sm:input-md input-bordered border-blue-500 w-full max-w-xs"
           />
         </div>
         {ongoingServices?.length > 0 ? (
@@ -77,46 +87,51 @@ const ServicesPage = () => {
                 {ongoingServices?.map((service, index) => (
                   <div
                     key={index}
-                    className="border rounded-sm relative pb-24 hover:shadow-lg"
+                    class="relative flex justify-between flex-col bg-clip-border rounded-xl bg-gradient-to-b text-white from-green-500 to-blue-500 shadow-gray-900/20 shadow-md w-full p-8 hover:scale-105 duration-300"
                   >
-                    <div
-                      className="px-4"
-                      style={{
-                        backgroundImage:
-                          "url('https://www.mobilecarwash.com/img/frontend/packages-img.png')",
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                        height: "160px",
-                      }}
-                    >
-                      <h4 className="text-center text-white font-bold text-xl pt-3">
+                    <div class="relative pb-8 m-0 mb-8 overflow-hidden text-center bg-transparent border-b rounded-none shadow-none bg-clip-border border-white">
+                      <p class="block font-sans text-sm antialiased font-normal leading-normal uppercase">
                         {service?.type}
-                      </h4>
-                      <h4 className="text-center text-white font-bold text-xl md:text-2xl pt-9">
-                        ${service?.price}
-                      </h4>
+                      </p>
+                      <h1 class="flex justify-center gap-1 mt-6 font-sans antialiased font-normal tracking-normal text-2xl md:text-3xl lg:text-5xl">
+                        <span class="mt-2 text-xl lg:text-2xl">$</span>
+                        {service?.price}
+                      </h1>
                     </div>
-                    <div className="px-4 bg-[#19457c] text-white my-4">
-                      <h4 className="font-bold text-xl py-3">Facilities</h4>
+                    <div class="p-0">
+                      <ul class="flex flex-col gap-4">
+                        {service?.facilities?.map((facility, index) => (
+                          <li key={index} class="flex items-center gap-4">
+                            <span class="p-1 border rounded-full border-white/20 bg-white/20">
+                              <TiTick />
+                              {/* <RxCross2 /> */}
+                            </span>
+                            <p class="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                              {facility}
+                            </p>
+                          </li>
+                        ))}
+                        {facilities?.map(
+                          (f, index) =>
+                            !service?.facilities?.includes(f) && (
+                              <li key={index} class="flex items-center gap-4">
+                                <span class="p-1 border rounded-full border-white/20 bg-white/20">
+                                  <RxCross2 />
+                                </span>
+                                <p class="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                                  {f}
+                                </p>
+                              </li>
+                            )
+                        )}
+                      </ul>
                     </div>
-                    <div className="px-4">
-                      {service?.facilities?.map((facility, index) => (
-                        <li
-                          key={index}
-                          className="flex items-center gap-4 mb-1"
-                        >
-                          <TiTick className="text-green-500" />
-                          {facility}
-                        </li>
-                      ))}
-                    </div>
-                    <div className="absolute bottom-0 left-0 border-t w-full text-center p-4">
+                    <div class="p-0 mt-12">
                       <button
                         onClick={() => handleAddToCart(service?.id)}
-                        className="btn btn-md bg-[#19457c] hover:bg-[#0f2b4e] text-white rounded-full px-10"
+                        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-blue-600 hover:bg-blue-700 shadow-md shadow-blue-gray-500/10 hover:shadow-lg hover:shadow-blue-gray-500/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
                       >
-                        Add To Cart
+                        Book Now
                       </button>
                     </div>
                   </div>
