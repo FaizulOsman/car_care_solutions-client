16bba2095b8e5482d592f5d23b18242e44d971d9
FaizulOsman <faizulosmanrabby@gmail.com>
2024-01-03 21:29:35 +0600

    update: survey section updated

--- src/components/UI/Survey.jsx
+++ src/components/UI/Survey.jsx
@@ -23,26 +23,26 @@ const Statistic = () => {
           />
           <div className="grid grid-cols-2 lg:grid-cols-4">
             <div className="text-center mt-16">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-green-500">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-[#fc4949]">
                 <CountUp scrollSpyDelay={2} enableScrollSpy={true} end={9} />+
               </h1>
               <p className="mt-3">Total Services</p>
             </div>
             <div className="text-center mt-16">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-green-500">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-[#fc4949]">
                 <CountUp scrollSpyDelay={2} enableScrollSpy={true} end={1200} />
                 +
               </h1>
               <p className="mt-3">Total Users</p>
             </div>
             <div className="text-center mt-16">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-green-500">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-[#fc4949]">
                 <CountUp scrollSpyDelay={2} enableScrollSpy={true} end={90} />%
               </h1>
               <p className="mt-3">Clients are from references</p>
             </div>
             <div className="text-center mt-16">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-green-500">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-[#fc4949]">
                 <CountUp scrollSpyDelay={2} enableScrollSpy={true} end={100} />+
               </h1>
               <p className="mt-3">Booked Services</p>
