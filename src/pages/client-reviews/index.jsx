5b875ab5e4097fcf057d546ec553b1f5d768c51a
FaizulOsman <faizulosmanrabby@gmail.com>
2024-01-07 00:34:53 +0600

    update: client review page updated

--- src/pages/client-reviews/index.jsx
+++ src/pages/client-reviews/index.jsx
@@ -35,98 +35,108 @@ const ClientReviewsPage = () => {
           Client Testimonials Highlighting the Unmatched Quality and Exceptional
           Service in Car Maintenance.
         </p>
-        {getAllReview?.data?.length > 0 ? (
-          <Swiper
-            breakpoints={{
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 15,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 15,
-              },
-            }}
-            freeMode={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[FreeMode, Pagination]}
-            className="max-w-[100%]"
-          >
-            {getAllReview?.data.map((review, index) => (
-              <SwiperSlide key={index}>
-                <div className="relative bg-gray-100 hover:bg-gray-200 border flex flex-col mb-20 group shadow-lg rounded-xl px-6 pt-8 pb-10 cursor-pointer">
-                  <div
-                    className="absolute inset-0 bg-cover bg-center"
-                    style={{
-                      backgroundImage: `url(${review.backgroundImage})`,
-                    }}
-                  />
-                  <div className="relative flex flex-col gap-3 justify-center items-center">
-                    <img
-                      className="absolute -bottom-[60%] rounded-full w-[90px] h-[90px] border-2 border-white"
-                      src={`https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(${
-                        index + 1
-                      }).jpg`}
-                      alt="avatar"
-                    />
-                    <div className="text-center w-full shrink-0 grow-0 basis-auto px-3">
-                      <p className="mb-2 font-bold">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 96 960 960"
-                          className="inline-block w-8 text-blue-500"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z"
-                          />
-                        </svg>
-                      </p>
-                      <p className="mb-2">
-                        {'"'}
-                        {review.review}
-                        {'"'}
-                      </p>
-                      <ul className="mb-8 flex justify-center">
-                        {Array.from({ length: review?.rating }).map((_, i) => (
-                          <li key={i}>
+        {getAllReview ? (
+          <>
+            {getAllReview?.data?.length > 0 ? (
+              <Swiper
+                breakpoints={{
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 15,
+                  },
+                  1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 15,
+                  },
+                }}
+                freeMode={true}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="max-w-[100%]"
+              >
+                {getAllReview?.data.map((review, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="relative bg-gray-100 hover:bg-gray-200 border flex flex-col mb-20 group shadow-lg rounded-xl px-6 pt-8 pb-10 cursor-pointer">
+                      <div
+                        className="absolute inset-0 bg-cover bg-center"
+                        style={{
+                          backgroundImage: `url(${review.backgroundImage})`,
+                        }}
+                      />
+                      <div className="relative flex flex-col gap-3 justify-center items-center">
+                        <img
+                          className="absolute -bottom-[60%] rounded-full w-[90px] h-[90px] border-2 border-white"
+                          src={`https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(${
+                            index + 1
+                          }).jpg`}
+                          alt="avatar"
+                        />
+                        <div className="text-center w-full shrink-0 grow-0 basis-auto px-3">
+                          <p className="mb-2 font-bold">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 96 960 960"
-                              className="w-5 text-warning"
+                              className="inline-block w-8 text-blue-500"
                             >
                               <path
                                 fill="currentColor"
-                                d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                                d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z"
                               />
                             </svg>
-                          </li>
-                        ))}
-                        {Array.from({ length: 5 - review?.rating }).map(
-                          (_, i) => (
-                            <li key={i}>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 96 960 960"
-                                className="w-5 text-gray-400"
-                              >
-                                <path
-                                  fill="currentColor"
-                                  d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
-                                />
-                              </svg>
-                            </li>
-                          )
-                        )}
-                      </ul>
+                          </p>
+                          <p className="mb-2">
+                            {'"'}
+                            {review.review}
+                            {'"'}
+                          </p>
+                          <ul className="mb-8 flex justify-center">
+                            {Array.from({ length: review?.rating }).map(
+                              (_, i) => (
+                                <li key={i}>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 96 960 960"
+                                    className="w-5 text-warning"
+                                  >
+                                    <path
+                                      fill="currentColor"
+                                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                                    />
+                                  </svg>
+                                </li>
+                              )
+                            )}
+                            {Array.from({ length: 5 - review?.rating }).map(
+                              (_, i) => (
+                                <li key={i}>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 96 960 960"
+                                    className="w-5 text-gray-400"
+                                  >
+                                    <path
+                                      fill="currentColor"
+                                      d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"
+                                    />
+                                  </svg>
+                                </li>
+                              )
+                            )}
+                          </ul>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <h2 className="text-2xl font-bold text-red-500 text-center py-10">
+                No Data Found
+              </h2>
+            )}
+          </>
         ) : (
           <Loader />
         )}
