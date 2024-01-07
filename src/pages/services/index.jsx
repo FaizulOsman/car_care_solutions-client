d9528b545347b21f3d5b517f4dc652589b72fa55
FaizulOsman <faizulosmanrabby@gmail.com>
2024-01-07 20:28:36 +0600

    update: service page updated

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useGetAllServiceQuery } from "../../redux/service/serviceApi";
-import { TiTick } from "react-icons/ti";
 import RootLayout from "../../layouts/RootLayout";
 import toast from "react-hot-toast";
 import { useCreateAddToCartMutation } from "../../redux/addToCart/addToCartApi";
 import Loader from "../../components/UI/Loader";
-import { RxCross2 } from "react-icons/rx";
 import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import SectionHeader from "../../components/UI/SectionHeader";
+import SectionTopHeader from "../../components/UI/SectionTopHeader";
+import { FaCheckCircle } from "react-icons/fa";
 
 const jwt = require("jsonwebtoken");
 
@@ -49,15 +49,11 @@ const ServicesPage = () => {
   };
 
   let facilities = [
-    "Exterior Hand Wash",
-    "Rims + Tire Shine",
-    "Full Vacuum",
-    "Shampoo Seats",
-    "Interior Wipe Down",
+    "Lights And Accessories",
+    "Maintenance Package",
+    "Exhaust System Services",
+    "Brakes & Brake Repair",
     "Deep Leather Cleaning",
-    "Deluxe Wax Protection",
-    "Executive Interior Detailing",
-    "Full exterior body clay treatment",
   ];
 
   useEffect(() => {
@@ -76,18 +72,24 @@ const ServicesPage = () => {
 
   return (
     <div className="w-11/12 max-w-[1200px] mx-auto mb-20">
-      <SectionHeader
-        title="Our Services"
-        styles="text-2xl sm:text-3xl lg:text-4xl text-center pb-5"
-      />
-      <p className="text-center max-w-lg mx-auto pb-5">
-        Feel free to contact us at any time. We have a dedicated team to provide
-        you the best support.
-      </p>
+      <div className="flex items-center justify-center flex-col">
+        <SectionTopHeader
+          title="PRICING PLAN"
+          styles="text-md sm:text-lg lg:text-xl text-center text-[#EB3300] pb-3"
+        />
+        <SectionHeader
+          title="Quality Services"
+          styles="text-2xl sm:text-3xl lg:text-4xl text-center pb-5"
+        />
+        <p className="max-w-[500px] mx-auto mb-5 leading-relaxed text-gray-600 text-center pb-5">
+          Feel free to contact us at any time. We have a dedicated team to
+          provide you the best support.
+        </p>
+      </div>
       <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mb-7">
         <div className="w-56 sm:w-64 mx-auto sm:mx-0">
           <input
-            className="bg-transparent shadow appearance-none border border-blue-500 rounded w-full py-2 px-3 text-blue-500 leading-tight focus:outline-none focus:shadow-outline"
+            className="bg-transparent shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="Search for services"
             onChange={(e) => setSearchValue(e.target.value)}
@@ -98,7 +100,7 @@ const ServicesPage = () => {
             onChange={(e) => {
               setStatus(e.target.value);
             }}
-            className="select select-bordered border border-blue-500 rounded px-3 text-gray-400 font-normal select-xs sm:select-sm max-w-xs"
+            className="select select-bordered border border-gray-400 rounded px-3 text-gray-400 font-normal select-xs sm:select-sm max-w-xs"
           >
             <option value="ongoing">Filter By Status</option>
             <option value="ongoing">Ongoing</option>
@@ -138,24 +140,38 @@ const ServicesPage = () => {
               {allService?.data?.map((service, index) => (
                 <div
                   key={index}
-                  className="relative flex justify-between flex-col bg-clip-border border border-blue-200 rounded-xl shadow-blue-300 shadow-md hover:shadow-2xl w-full p-8 hover:scale-105 duration-300"
+                  className={`relative flex justify-between flex-col bg-clip-border border border-gray-200 rounded-sm shadow-gray-300 shadow-md hover:shadow-2xl w-full p-8 hover:scale-105 duration-300 ${
+                    service?.type === "Standard Package" &&
+                    "bg-[#0a0a0a] text-white"
+                  }`}
+                  style={{
+                    backgroundImage:
+                      "url(https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/price_bg_1.png",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                  }}
                 >
-                  <div className="relative pb-8 m-0 mb-8 overflow-hidden text-center bg-transparent border-b rounded-none shadow-none bg-clip-border border-blue-300">
-                    <p className="block font-sans text-sm antialiased font-semibold text-blue-500  leading-normal uppercase">
+                  <div className="relative pb-8 m-0 mb-8 overflow-hidden bg-transparent border-b rounded-none">
+                    <p className="block font-sans text-2xl antialiased font-semibold mb-4">
                       {service?.type}
                     </p>
-                    <h1 className="text-blue-500 flex justify-center gap-1 mt-6 font-sans antialiased font-normal tracking-normal text-2xl md:text-3xl lg:text-5xl">
-                      <span className="mt-2 text-xl lg:text-2xl">$</span>
-                      {service?.price}
-                    </h1>
+                    <div className="flex justify-between items-center">
+                      <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">
+                        ${service?.price}
+                      </h1>
+                      {service?.type === "Standard Package" && (
+                        <span className="bg-[#EB3300] text-white px-3 pb-[2px] rounded-full ">
+                          25% Save
+                        </span>
+                      )}
+                    </div>
                   </div>
                   <div className="p-0">
                     <ul className="flex flex-col gap-4">
                       {service?.facilities?.map((facility, index) => (
                         <li key={index} className="flex items-center gap-4">
-                          <span className="p-1 border rounded-full border-gray-300 bg-gray-50">
-                            <TiTick className="text-blue-500" />
-                          </span>
+                          <FaCheckCircle className="text-[#EB3300]" />
                           <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
                             {facility}
                           </p>
@@ -165,9 +181,7 @@ const ServicesPage = () => {
                         (f, index) =>
                           !service?.facilities?.includes(f) && (
                             <li key={index} className="flex items-center gap-4">
-                              <span className="p-1 border rounded-full border-gray-300 bg-gray-50">
-                                <RxCross2 className="text-red-500" />
-                              </span>
+                              <FaCheckCircle className="text-gray-500" />
                               <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
                                 {f}
                               </p>
@@ -179,7 +193,11 @@ const ServicesPage = () => {
                   <div className="p-0 mt-12">
                     <button
                       onClick={() => handleAddToCart(service?.id)}
-                      className="w-full btn border hover:bg-white hover:border-blue-500 hover:text-blue-500 bg-blue-500 text-white"
+                      className={`w-full btn border border-[#EB3300] ${
+                        service?.type === "Standard Package"
+                          ? "hover:bg-white hover:text-[#EB3300] bg-[#EB3300] text-white"
+                          : " bg-white text-[#EB3300] hover:bg-[#EB3300] hover:text-white"
+                      }`}
                       disabled={status === "upcoming" && true}
                     >
                       Add To Wishlist
@@ -189,7 +207,7 @@ const ServicesPage = () => {
               ))}
             </div>
           ) : (
-            <h1 className="text-xl sm:text-2xl py-20 text-center text-red-500">
+            <h1 className="text-xl sm:text-2xl py-20 text-center text-[#EB3300]">
               No data found
             </h1>
           )}
