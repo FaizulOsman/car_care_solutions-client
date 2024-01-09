a6f58e2f74c89f46f102b6176d3870f7cca6bb41
FaizulOsman <faizulosmanrabby@gmail.com>
2024-01-09 12:45:15 +0600

    update: about us updated

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -4,6 +4,7 @@ import SectionHeader from "../../components/UI/SectionHeader";
 import RootLayout from "../../layouts/RootLayout";
 import { FaArrowRightLong } from "react-icons/fa6";
 import Link from "next/link";
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
@@ -11,9 +12,11 @@ const AboutUs = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
         <div className="relative">
           <div className="z-20">
-            <img
+            <Image
               className="w-[75%] sm:w-[50%] lg:w-[75%] z-20"
-              src="https://i.ibb.co/RP0Tpk5/about-2.png"
+              src="https://i.ibb.co/JvXTM4z/about-2.png"
+              height={600}
+              width={600}
               alt=""
             />
           </div>
@@ -21,18 +24,24 @@ const AboutUs = () => {
             id="about-us-animation-left-right"
             className="w-[50%] sm:w-[40%] lg:w-[50%] absolute top-[15%] right-5 z-20"
           >
-            <img
+            <Image
               className="w-full border-4 border-white"
-              src="https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/about_3.png"
+              src="https://i.ibb.co/ZM7SxR8/about-3.png"
+              height={600}
+              width={600}
+              alt=""
             />
           </div>
           <div
             id="about-us-animation-top-bottom"
             className="w-[50%] sm:w-[30%] lg:w-[53%] absolute top-2 right-1 sm:right-[20%] sm:top-[6%] lg:right-1 lg:-top-4"
           >
-            <img
+            <Image
               className="w-full"
-              src="https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/about_dots.png"
+              src="https://i.ibb.co/6ZJCGcj/about-dots.png"
+              height={600}
+              width={600}
+              alt=""
             />
           </div>
         </div>
@@ -55,11 +64,13 @@ const AboutUs = () => {
           <div className="flex justify-between items-center gap-8 mb-10 lg:mb-5 xl:mb-10">
             <div>
               <div className="flex gap-6 items-center pb-5 mb-5 border-b">
-                <img
-                  src="https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/success.svg"
+                <Image
+                  src="https://i.ibb.co/kQPhysG/award.png"
+                  height={60}
+                  width={60}
                   alt=""
                 />
-                <div className="">
+                <div className="hover:text-red-600">
                   <h3 className="text-xl font-bold mb-2">
                     Award Wining Company
                   </h3>
@@ -69,11 +80,13 @@ const AboutUs = () => {
                 </div>
               </div>
               <div className="flex gap-6 items-center">
-                <img
-                  src="https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/phone_1.svg"
+                <Image
+                  src="https://i.ibb.co/NWPYgX9/support.png"
+                  height={60}
+                  width={60}
                   alt=""
                 />
-                <div className="">
+                <div className="hover:text-red-600">
                   <h3 className="text-xl font-bold mb-2">Friendly Support</h3>
                   <p className="text-[#4B4B4B]">
                     Award-winning companies often excel in areas such as
@@ -83,8 +96,7 @@ const AboutUs = () => {
             </div>
             <div
               style={{
-                backgroundImage:
-                  "url(	https://themeholy.com/wordpress/malen/wp-content/uploads/2023/05/about_img2.png)",
+                backgroundImage: "url(https://i.ibb.co/bRmbZrx/about-img2.png)",
                 backgroundRepeat: "no-repeat",
               }}
               className="hidden w-[164px] h-[214px] text-white sm:flex flex-col justify-center px-5 gap-2"
